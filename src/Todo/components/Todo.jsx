535cdee0645b4b88ad1b99d5959a2ee2417b12ea
ogatakatsuya <katsuya151225@icloud.com>
2024-04-09 07:45:48 +0900

    modify mordal

--- src/Todo/components/Todo.jsx
+++ src/Todo/components/Todo.jsx
@@ -97,6 +97,34 @@ const Todo = () => {
               <IconButton icon={<AiOutlineForm />} isRound onClick={() => openEditModal(task.id)}>
                 編集
               </IconButton>
+              <Modal isOpen={isOpen} onClose={onClose}>
+                  <ModalOverlay />
+[                      <ModalContent>
+                  <ModalHeader>編集する</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalFooter>
+                  <Input
+                  placeholder="タスクを編集する"
+                  _placeholder={{ opacity: "0.3", color: "gray.500" }}
+                  size="lg"
+                  p={3}
+                  bgColor="white"
+                  variant="flushed"
+                  value={updatedTodo}
+                  onChange={(e) => setUpdatedTodo(e.target.value)}
+                  />
+                  <Button
+                  colorScheme="blue"
+                  size="md"
+                  bgColor="white"
+                  variant="outline"
+                  px={7}
+                  type="submit"
+                  onClick={() => updateTodo(selectedTodoId)}
+                  >更新</Button>
+                  </ModalFooter>
+              </ModalContent>]
+              </Modal>
               <IconButton
                 icon={<AiOutlineDelete />} 
                 isRound bgColor="cyan.100"
